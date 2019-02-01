e02e41aaa14548aebcfb44b54cda4f13dddac2f0
Nethelbert Blasse <nethelbert.blasse@gmail.com>
2019-02-01 15:38:45 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -2,10 +2,10 @@ function insertSpan(){
     let pElmt = $('<p></p>').appendTo('#loader');
 
     let words = 'loading...';
-    let arry = words.split('');
+    let wordsArry = words.split('');
 
-    for(let i=0; i < arry.length; i++){
-        let spanLetter = $(`<span>${arry[i]}</span>`)
+    for(let i=0; i < wordsArry.length; i++){
+        let spanLetter = $(`<span>${wordsArry[i]}</span>`)
         .css('animationDelay',`0.${i}s`)
         .appendTo('p');s
     }
